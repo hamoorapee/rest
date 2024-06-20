41e39e598f242235b5fa085bbfb2e67d50583d3e
hamoorapee <kola927@list.ru>
2024-06-20 20:51:46 +0500

    added models

--- src/test/java/tests/demoWebShopTests.java
+++ src/test/java/tests/demoWebShopTests.java
@@ -2,29 +2,33 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class demoWebShopTests {
+    String authCookieName = "NOPCOMMERCE.AUTH",
+    email = "kola@mail.com",
+    password = "Rjkz2002";
 
     @BeforeAll
-    static void setUp(){
+    static void setUp() {
         Configuration.baseUrl = "https://demowebshop.tricentis.com";
         Configuration.headless = true;
         RestAssured.baseURI = "https://demowebshop.tricentis.com";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @Test
@@ -92,18 +96,7 @@ public class demoWebShopTests {
     @Test
     void addToOldCartAsAuthorizedTest() {
 
-        String authCookie = given()
-                .contentType("application/x-www-form-urlencoded")
-                .formParam("Email", "kola@mail.com")
-                .formParam("Password", "Rjkz2002")
-                .log().all()
-                .when()
-                .post("/login")
-                .then()
-                .log().all()
-                .statusCode(302)
-                .extract()
-                .cookie("NOPCOMMERCE.AUTH");
+        String authCookie = getAuthCookie(email,password);
 
         String body = "product_attribute_72_5_18=53" +
                 "&product_attribute_72_6_19=54" +
@@ -170,4 +163,64 @@ public class demoWebShopTests {
         $(".cart-qty").shouldHave(text(cartSize));
     }
 
+    @Test
+    void addToCartWithAllureTest() {
+
+        String authCookieValue = getAuthCookie(email, password);
+
+        String body = "product_attribute_72_5_18=53" +
+                "&product_attribute_72_6_19=54" +
+                "&product_attribute_72_3_20=57" +
+                "&addtocart_72.EnteredQuantity=1";
+
+        String cartSize = getCartSize(body, authCookieValue);
+
+        step("Open minimal content, bc cookie can be set when site is opened", () ->
+                open("/Themes/DefaultClean/Content/images/free-shipping.png"));
+
+        step("Set cookies to browser", () -> {
+
+            Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+            WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+        });
+        step("Open main page", () ->
+            open(""));
+        step("Check cart size",() ->
+            $(".cart-qty").shouldHave(text(cartSize)));
+    }
+
+    @Step("Get authorization cookie")
+    String getAuthCookie(String email, String password) {
+        return given()
+                .filter(new AllureRestAssured())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("Email", email)
+                .formParam("Password", password)
+                .log().all()
+                .when()
+                .post("/login")
+                .then()
+                .log().all()
+                .statusCode(302)
+                .extract()
+                .cookie(authCookieName);
+    }
+
+    @Step("Get authorization cookie")
+    String getCartSize(String body, String authCookieValue) {
+        return given()
+                .filter(new AllureRestAssured())
+                .contentType("application/x-www-form-urlencoded")
+                .cookie(authCookieName, authCookieValue)
+                .body(body)
+                .log().all()
+                .when()
+                .post("/addproducttocart/details/72/1")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", is(true))
+                .extract()
+                .path("updatetopcartsectionhtml");
+    }
 }
